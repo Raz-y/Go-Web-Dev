89f020f3fb7424f4fea3edbc63b2c53b1bedd3eb
Raz Yehiel <raz.y054@gmail.com>
2024-09-22 15:51:04 +0300

    refactor homehandler to use home template

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome</h1>")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tplPath := filepath.Join("templates", "home.gohtml")
+	tpl, err := template.ParseFiles(tplPath)
+	if err != nil {
+		log.Printf("Parsing template %v,", err)
+		http.Error(w, "there was an error parsing the template.", http.StatusInternalServerError)
+		return
+	}
 
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		panic(err) // TODO: Remove panic
+
+	}
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
