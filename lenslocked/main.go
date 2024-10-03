783b0cfe8a430bb07380398c22c757550aa55574
Raz Yehiel <raz.y054@gmail.com>
2024-10-03 13:17:12 +0300

    Add and executeTemplage func and update contact page

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -10,10 +10,9 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tplPath := filepath.Join("templates", "home.gohtml")
-	tpl, err := template.ParseFiles(tplPath)
+	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
 		log.Printf("Parsing template %v,", err)
 		http.Error(w, "there was an error parsing the template.", http.StatusInternalServerError)
@@ -22,13 +21,20 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 	err = tpl.Execute(w, nil)
 	if err != nil {
-		panic(err) // TODO: Remove panic
-
+		log.Printf("Execute template %v,", err)
+		http.Error(w, "there was an error executing the template.", http.StatusInternalServerError)
+		return
 	}
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tplPath)
+}
+
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Contact</h1>")
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func main() {
